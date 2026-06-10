d71f8b98029ea956441acfb0a28e08794d78de80
k-sapp <kalenasapp@gmail.com>
2026-06-09 18:27:13 -0700

    Deleting duplicate functions

--- R/classify_flood_zone.R
+++ R/classify_flood_zone.R
@@ -14,15 +14,4 @@ classify_flood_zone <- function(elevation_grid, rise_m) {
   elevation_grid$flooded <- elevation_grid$elevation_m <= rise_m
   return(elevation_grid)
 }
-# classify_flood_zone.R
-# Purpose: Classify which grid cells are flooded for a given sea level rise amount
-# Inputs:  elevation_grid (data.frame with column elevation_m)
-#          rise_m (numeric - how much sea level has risen in meters)
-# Output:  elevation_grid data.frame
 
-classify_flood_zone <- function(elevation_grid, rise_m) {
-
-  elevation_grid$flooded <- elevation_grid$elevation_m <= rise_m
-
-  return(elevation_grid)
-}
--- R/compute_affected_area.R
+++ R/compute_affected_area.R
@@ -16,20 +16,3 @@ compute_affected_area <- function(flood_mask, grid_cell_size_m) {
   area_km2  <- area_m2 / 1e6
   return(area_km2)
 }
-# compute_affected_area.R
-# Purpose: Calculate total flooded area in km²
-# Inputs:  flood_mask (data.frame with a 'flooded' TRUE/FALSE column)
-#          grid_cell_size_m (numeric - size of each cell in meters)
-# Output:  numeric - total flooded area in km²
-
-compute_affected_area <- function(flood_mask, grid_cell_size_m) {
-
-  # count how many cells are flooded
-  n_flooded <- sum(flood_mask$flooded)
-
-  # convert cell count to area in km²
-  area_m2  <- n_flooded * (grid_cell_size_m ^ 2)
-  area_km2 <- area_m2 / 1e6
-
-  return(area_km2)
-}
